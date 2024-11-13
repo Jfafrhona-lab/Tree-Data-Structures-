fd39a4d8620e4e86ae4ca666a85c038e617c9fd1
Jfafrhona-lab <rhonamariecapuyan@gmail.com>
2024-11-13 23:20:14 +0800

    Add files via upload

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,5 +1,64 @@
-public class Main {
-	public static void main(String[] args) {
-		
-	}
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+
+    BinaryTree() {
+        root = null;
+    }
+
+    void addNode(int data) {
+        root = addRecursive(root, data);
+    }
+
+    Node addRecursive(Node current, int data) {
+        if (current == null) {
+            return new Node(data);
+        }
+
+        if (data < current.data) {
+            current.left = addRecursive(current.left, data);
+        } else if (data > current.data) {
+            current.right = addRecursive(current.right, data);
+        } else {
+            // value already exists
+            return current;
+        }
+
+        return current;
+    }
+
+
+    void inorderTraversal(Node node) {
+        if (node != null) {
+            inorderTraversal(node.left);
+            System.out.print(node.data + " ");
+            inorderTraversal(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.addNode(8);
+        tree.addNode(3);
+        tree.addNode(10);
+        tree.addNode(1);
+        tree.addNode(6);
+        tree.addNode(14);
+        tree.addNode(4);
+        tree.addNode(7);
+        tree.addNode(13);
+
+        System.out.println("Inorder traversal of the constructed tree is ");
+        tree.inorderTraversal(tree.root);
+    }
 }
